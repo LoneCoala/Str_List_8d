14ba45c08aeb5d429f0ec7a6a3e5ddedcff118b2
Даниил Тюрин <tjurin.daniil@rambler.ru>
2022-02-23 08:38:43 +0700

    Доделал

--- Str_List_8d/Program.cs
+++ Str_List_8d/Program.cs
@@ -7,23 +7,79 @@ namespace Str_List_8d
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("начальный список:");
-            List<string> people = new List<string>() { "Tom", "Bob", "Sam", "Aaa", "12321", "Але", "ксандр" };
+            List<string> people = new List<string>() { "Tom", "Bob", "Sam" };
+            Console.WriteLine("Начальный список:");
             for (int i = 0; i < people.Count; i++)
             {
                 Console.Write(people[i] + " ");
             }
+            Console.WriteLine("Сколько элементов добавить в список?");
+            int n = 0;
 
-            RevList(people);
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Введена неверная форма числа! Попробуйте снова.");
+            }
 
-            Console.WriteLine("\nновый список:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Введите новый элемент списка №{i + 1}:");
+                people.Add(Console.ReadLine());
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("Новый список до изменений:");
             for (int i = 0; i < people.Count; i++)
             {
                 Console.Write(people[i] + " ");
             }
+            Console.WriteLine("\nПеренести последний элемент в начало, добавить элементы или закончить выполнение программы?\n1.Перенести\n2.Добавить\n0.Закончить");
+            string choose = "";
+            while (choose!= "0")
+            {
+                choose = Console.ReadLine();
+                if (choose == "2")
+                {
+                    Console.WriteLine("Начальный список:");
+                    for (int i = 0; i < people.Count; i++)
+                    {
+                        Console.Write(people[i] + " ");
+                    }
+                    Console.WriteLine("Сколько элементов добавить в список?");
+                    n = 0;
+
+                    while (!int.TryParse(Console.ReadLine(), out n))
+                    {
+                        Console.WriteLine("Введена неверная форма числа! Попробуйте снова.");
+                    }
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.Write($"Введите новый элемент списка №{i + 1}:");
+                        people.Add(Console.ReadLine());
+                        Console.WriteLine("");
+                    }
+                }
+                if (choose == "1")
+                {
+                    MoveToBeginningOfList(people);
+                }
+                if (choose == "0")
+                {
+                    break;
+                }
+                Console.WriteLine("\nНовый список:");
+                for (int i = 0; i < people.Count; i++)
+                {
+                    Console.Write(people[i] + " ");
+                }
+                Console.WriteLine("\nПеренести последний элемент в начало, добавить элементы или закончить выполнение программы?\n1.Перенести\n2.Добавить\n0.Закончить");
+            }
+
+            Console.WriteLine("Конец работы программы");
 
 
-            List<string> RevList(List<string> list)
+            List<string> MoveToBeginningOfList(List<string> list)
             {
                 string str = list[list.Count - 1];
                 list.Reverse();
