61404fb5246b489b59ca639e6de12a9a90bfcbdb
Даниил Тюрин <tjurin.daniil@rambler.ru>
2022-02-23 07:39:58 +0700

    Добавьте файлы проекта.

---
+++ Str_List_8d/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Str_List_8d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("начальный список:");
+            List<string> people = new List<string>() { "Tom", "Bob", "Sam", "Aaa", "12321", "Але", "ксандр" };
+            for (int i = 0; i < people.Count; i++)
+            {
+                Console.Write(people[i] + " ");
+            }
+
+            RevList(people);
+
+            Console.WriteLine("\nновый список:");
+            for (int i = 0; i < people.Count; i++)
+            {
+                Console.Write(people[i] + " ");
+            }
+
+
+            List<string> RevList(List<string> list)
+            {
+                string str = list[list.Count - 1];
+                list.Reverse();
+                list.Add(str);
+                list.Reverse();
+                list.RemoveAt(list.Count - 1);
+                return list;
+            }
+        }
+    }
+}
